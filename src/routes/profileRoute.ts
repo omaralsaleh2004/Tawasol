a621fe891c4d5e805dffd5910dfe9f1d6584466b
Omar <Omar@example.com>
2025-06-15 23:30:24 +0300

    Added => get user profile feature + delete profille feature

--- src/routes/profileRoute.ts
+++ src/routes/profileRoute.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { validateJWT } from "../middlewares/validateJWT";
 import { ExtendRequest } from "../types/ExtendRequest";
 import { addorEditPost } from "../services/profileService";
+import { profileModel } from "../models/profileModel";
+import { userModel } from "../models/userModel";
 
 const router = express.Router();
 
@@ -39,5 +41,57 @@ router.post("/", validateJWT, async (req: ExtendRequest, res) => {
   } catch {
     res.status(500).json("something went wrong !");
   }
+});
+
+router.get("/me", validateJWT, async (req: ExtendRequest, res) => {
+  try {
+    const userId = req.user._id;
+    const profile = await profileModel
+      .findOne({ userId })
+      .populate("userId", "firstName");
+
+    if (!profile) {
+      res.status(400).json("There is no Profile for this user");
+      return;
+    }
+
+    res.status(200).json(profile);
+  } catch {
+    res.status(500).json("something went wrong !");
+  }
+});
+
+router.get("/", validateJWT, async (req: ExtendRequest, res) => {
+  try {
+    const profiles = await profileModel.find().populate("userId", "firstName");
+
+    res.status(200).json(profiles);
+  } catch {
+    res.status(500).json("something went wrong !");
+  }
+});
+
+router.get("/:user_id", validateJWT, async (req: ExtendRequest, res) => {
+  try {
+    const profile = await profileModel
+      .findOne({ userId: req.params.user_id })
+      .populate("userId", "firstName");
+
+    if (!profile) {
+      res.status(400).json("There is no Profile for the given user");
+      return;
+    }
+
+    res.status(200).json(profile);
+  } catch {
+    res.status(500).json("something went wrong !");
+  }
+});
+
+router.delete("/", validateJWT, async (req: ExtendRequest, res) => {
+  //delete post , profile , user
+  //TODO : Include deleting posts
+  await profileModel.findOneAndDelete({ userId: req.user._id });
+  await userModel.findByIdAndDelete({ _id: req.user._id });
 });
 export default router;
