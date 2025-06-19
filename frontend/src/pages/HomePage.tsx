75bd63f0a085c3bbed81b70663e27248b1dc05f7
Omar <Omar@example.com>
2025-06-19 17:19:40 +0300

    Added HomePage component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage() {
-  return <></>;
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const handleSignup = () => {
+    navigate("/signup");
+  };
+
+  return (
+    <div className="Landing-page">
+      <h1>TawaSol</h1>
+      <div className="Landing-page-btn">
+        <button onClick={handleSignup}>Sign Up</button>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    </div>
+  );
 }
