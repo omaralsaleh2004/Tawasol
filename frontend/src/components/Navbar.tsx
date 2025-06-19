1f6c0a395b73cbdd8bea66a31f6409fd2f0e297b
Omar <Omar@example.com>
2025-06-19 15:32:32 +0300

    updated navbar logic

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,8 +2,18 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const handleHomePage = () => {
+    navigate("/");
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -14,10 +24,19 @@ export default function Navbar() {
             alignItems: "center",
           }}
         >
-          <Button color="inherit" sx={{ fontSize: "18px" }}>
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "#214cb0", fontSize: "18px" }}
+            onClick={handleHomePage}
+          >
             Tawasol
           </Button>
-          <Button color="inherit" sx={{ fontSize: "18px" }}>
+          <Button
+            color="primary"
+            variant="contained"
+            sx={{ backgroundColor: "#214cb0", fontSize: "18px" }}
+            onClick={handleLogin}
+          >
             Login
           </Button>
         </Toolbar>
